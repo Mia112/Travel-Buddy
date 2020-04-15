9b1c1422b8b21d2ce3608f40f66236f59c0cc6aa
Mia112 <msheriff112@gmail.com>
2020-04-15 16:21:56 -0400

    update

--- models/User.js
+++ models/User.js
@@ -1,22 +1,26 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-// Create the Schema
+
+// Create Schema
 const UserSchema = new Schema({
-	name: {
-		type: String,
-		required: true
-	},
-	email: {
-		type: String,
-		required: true
-	},
-	photo: {
-		type: String,
-		required: true,
-		default:
-			'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQiTpL5cDSP7-7PqBFa78ejlmBgZVO0hXm-KUQBej0mh4A0M8sO'
-	}
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  register_date: {
+    type: Date,
+    default: Date.now
+  }
 });
 
-module.exports = User = mongoose.model('users', UserSchema);
+module.exports = User = mongoose.model('user', UserSchema);
--- server.js
+++ server.js
@@ -1,12 +1,11 @@
 const express = require('express');
-const mongoose = require('mongoose');
-const passport = require('passport');
-const cookieSession = require('cookie-session');
-const path = require('path');
-
 const app = express();
-const auth = require('./routes/api/auth');
-const posts = require('./routes/api/posts');
+const path = require('path');
+const mongoose = require('mongoose');
+
+const Post = require('./controllers/posts');
+const User = require('./controllers/users');
+const Auth = require('./controllers/auth');
 
 require('dotenv').config();
 const cors = require('cors');
@@ -14,20 +13,10 @@ app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-require('./models/User.js');
-
-app.use(
-	cookieSession({
-		maxAge: 30 * 24 * 60 * 60 * 1000,
-		keys: ['secret']
-	})
-);
-
-const db = require('./config/keys').mongoURI;
-
 mongoose.set('useCreateIndex', true);
-
-mongoose.connect(db, { useUnifiedTopology: true, useNewUrlParser: true });
+const uri =
+	'mongodb+srv://dbMia:mia123@cluster0-kch9q.mongodb.net/test?retryWrites=true&w=majority';
+mongoose.connect(uri, { useUnifiedTopology: true, useNewUrlParser: true });
 
 const connection = mongoose.connection;
 connection.on('connected', () => {
@@ -37,23 +26,19 @@ connection.on('error', err => {
 	console.log('Mongoose default connection error: ' + err);
 });
 
-app.use(passport.initialize());
-app.use(passport.session());
-
-require('./config/passport');
-
-app.use('/api/auth', auth);
-app.use('/api/posts', posts);
+app.use('/api/Post', Post);
+app.use('/api/User', User);
+app.use('/api/auth', Auth);
 
 if (process.env.NODE_ENV === 'production') {
 	app.use(express.static('client/build'));
 
 	app.get('*', (req, res) => {
-		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+		res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
 	});
 }
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, function() {
 	console.log(`🌎 ==> API Server now listening on PORT ${PORT}!`);
 });
